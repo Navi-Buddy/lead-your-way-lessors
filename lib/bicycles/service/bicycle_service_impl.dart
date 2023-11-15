abd0ffead9e7f141524b07f476436d1180e0a103
JosR <josueramos17@hotmail.com>
2023-11-15 13:10:12 -0500

    chore: add method to bicycle service

--- lib/bicycles/service/bicycle_service_impl.dart
+++ lib/bicycles/service/bicycle_service_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:lyw_lessors/configuration/constants/environment.dart';
 import 'package:lyw_lessors/bicycles/domain/model/bicycle_model.dart';
@@ -16,16 +18,43 @@ class BicycleServiceImpl extends BicycleService {
 
   @override
   Future<List<Bicycle>?> getBicycles(int id) async {
-    final response = await dio.get('api/cyclescape/v1/users/${id.toString()}');
-    final List<dynamic> bicycleList = response.data['bicycles'];
-
-    final bicycles = bicycleList
-        .map((bicycleJson) => Bicycle.fromJson(bicycleJson))
-        .toList();
     try {
+      final response =
+          await dio.get('api/cyclescape/v1/users/${id.toString()}');
+      final List<dynamic> bicycleList = response.data['bicycles'];
+
+      final bicycles = bicycleList
+          .map((bicycleJson) => Bicycle.fromJson(bicycleJson))
+          .toList();
+
       return bicycles;
     } catch (e) {
       return null;
     }
+  }
+
+  @override
+  Future<String> addBicycle(int userId, Bicycle bicycle) async {
+    try {
+      final response = await dio
+          .post('api/cyclescape/v1/bicycles/${userId.toString()}', data: {
+        'bicycleName': bicycle.bicycleName,
+        'bicycleDescription': bicycle.bicycleDescription,
+        'bicyclePrice': bicycle.bicyclePrice,
+        'bicycleSize': bicycle.bicycleSize,
+        'bicycleModel': bicycle.bicycleModel,
+        'imageData': bicycle.imageData,
+        'latitudeData': bicycle.latitude,
+        'longitudeData': bicycle.longitude,
+      });
+      if (response.statusCode == 201) {
+        return 'Bike created successfully';
+      } else {
+        return 'An error has occured';
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return 'An error has occured';
   }
 }
