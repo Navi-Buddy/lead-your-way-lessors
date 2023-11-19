d46b830991275348f9700aabe45c8512c023113b
JosR <josueramos17@hotmail.com>
2023-11-19 15:31:10 -0500

    chore: add SliverAppBar to EditProfile screen

--- lib/profile/screen/edit_profile_screen.dart
+++ lib/profile/screen/edit_profile_screen.dart
@@ -22,9 +22,8 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   void navigateToProfile() {
     Navigator.pushReplacement(
       context,
-      MaterialPageRoute(
-        builder: (context) => const AppNavigator()),
-      );
+      MaterialPageRoute(builder: (context) => const AppNavigator()),
+    );
   }
 
   void onSubmit(User user) async {
@@ -46,21 +45,32 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Padding(
-          padding:
-              const EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 50),
-          child: ListView(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text('Edit your profile',
-                      style: TextStyle(fontSize: 24)),
-                  const SizedBox(height: 32),
-                  ProfileForm(onSubmit: onSubmit, user: widget.user)
-                ],
-              ),
-            ],
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              const SliverAppBar(
+                floating: false,
+                pinned: true,
+                backgroundColor: Colors.orange,
+              )
+            ];
+          },
+          body: Padding(
+            padding:
+                const EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 50),
+            child: ListView(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Edit your profile',
+                        style: TextStyle(fontSize: 24)),
+                    const SizedBox(height: 32),
+                    ProfileForm(onSubmit: onSubmit, user: widget.user)
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
