5ac0daaf600bf7774167867262cdb4b4e2046143
lucas1336 <lucasruiz1336@gmail.com>
2023-11-22 12:37:21 -0500

    fix: hero id

--- lib/bicycles/widgets/hero_image.dart
+++ lib/bicycles/widgets/hero_image.dart
@@ -11,19 +11,16 @@ class HeroImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: bike.id,
-      child: SizedBox(
-        height: 100,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: FadeInImage(
-            image: NetworkImage(bike.imageData),
-            placeholder: const AssetImage('assets/loading_image.gif'),
-            imageErrorBuilder: (context, error, stackTrace) {
-              return const Icon(Icons.error);
-            },
-          ),
+    return SizedBox(
+      height: 100,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: FadeInImage(
+          image: NetworkImage(bike.imageData),
+          placeholder: const AssetImage('assets/loading_image.gif'),
+          imageErrorBuilder: (context, error, stackTrace) {
+            return const Icon(Icons.error);
+          },
         ),
       ),
     );
